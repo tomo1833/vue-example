53dc79fcca9fde64cf1656efda042433ed4942e4
tomo1833 <m.tomo0912.tm@gmail.com>
2023-11-24 12:44:41 +0900

    example/1
    エラー処理実装

---
+++ vue-project/src/components/organisms/BaseOverlay.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useScreenStore } from '@/stores/screen'
+
+const screenStore = useScreenStore()
+</script>
+
+<template>
+  <div
+    v-show="screenStore.errorInfo.errorFlag"
+    class="absolute top-0 left-0 w-full h-full bg-slate-600 opacity-75"
+  >
+    <slot></slot>
+  </div>
+</template>
--- vue-project/src/components/templates/BaseTemplate.vue
+++ vue-project/src/components/templates/BaseTemplate.vue
@@ -2,16 +2,16 @@
 import BaseHeder from '@/components/organisms/BaseHeader.vue'
 import BaseAside from '@/components/organisms/BaseAside.vue'
 import BaseMain from '@/components/organisms/BaseMain.vue'
+import BaseOverlay from '../organisms/BaseOverlay.vue'
 </script>
 
 <template>
   <div class="bg-gray-100 h-screen overflow-hidden">
+    <BaseOverlay><slot name="oslot"></slot></BaseOverlay>
     <BaseHeder class="bg-blue-500 text-white p-4" />
     <div class="flex h-full">
       <BaseAside class="bg-gray-800 text-white p-4 w-96 hidden md:block overflow-y-auto" />
-      <BaseMain class="flex-1 p-4 overflow-y-auto">
-        <slot></slot>
-      </BaseMain>
+      <BaseMain class="flex-1 p-4 overflow-y-auto"><slot name="mslot"></slot></BaseMain>
     </div>
   </div>
 </template>
